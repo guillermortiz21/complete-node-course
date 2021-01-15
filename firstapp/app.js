bdadced3b79b11f2c315ee544c2f25fbe639284d
Guillermo Ortiz <guillermortiz21@gmail.com>
2021-01-14 19:58:32 -0600

    EventEmitter on and emit

--- firstapp/app.js
+++ firstapp/app.js
@@ -1,9 +1,21 @@
-const fs = require('fs');
+const EventEmitter = require('events');
+const emitter = new EventEmitter();
 
-// Get all files and folders in current folder
-console.log(fs.readdirSync('./'));
-
-fs.readdir('./', (err, files) => {
-  if(err) console.log(`Error: ${err}`);
-  else console.log(`Result: ${files}`);
+// We need to register a listener that listens to messageLogged events
+emitter.on("messageLogged", (arg) => {
+  // This function will be called when the event is raised
+  console.log(`Listener called ${JSON.stringify(arg)}`);
 });
+
+// Emit is used to raise and event
+// Emit is making a noise or producing something
+// We are sending a signal that something happened
+// You can pass arguments
+emitter.emit("messageLogged", {id: 1, url:"http://"});
+
+
+emitter.on("logging", (arg) => {
+  console.log(`Listened to logging: ${JSON.stringify(arg)}`);
+});
+
+emitter.emit("logging", {message:"Log this!"});
