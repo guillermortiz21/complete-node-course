02e759c0572ab2f8b2781b001927fad6ba319d17
Guillermo Ortiz <guillermortiz21@gmail.com>
2021-01-14 18:43:28 -0600

    module object

--- firstapp/app.js
+++ firstapp/app.js
@@ -1,6 +1,4 @@
-global.console.log("Hello");
-console.log("world");
-
-var myVar = "I am not in the global object";
-global.console.log(myVar);
-global.console.log(global.myVar); // Prints undefined
+/*
+This will show you information about this file like id, path, exports, parent or children.                               
+*/
+console.log(module);
