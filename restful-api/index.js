8ec869d24d0dfc104014a7ec0d4e5fc21698bbc9
Guillermo Ortiz <guillermortiz21@gmail.com>
2021-01-18 10:58:06 -0600

    Serve public files

--- restful-api/index.js
+++ restful-api/index.js
@@ -5,6 +5,8 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({extended: true})); // for form-urlencoded payloads
+app.use(express.static('public'));
 app.use(logger);
 app.use(authentication);
  
