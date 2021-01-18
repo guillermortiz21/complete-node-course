6b45de204ba34d885c70f3f085d46d779ab3c1de
Guillermo Ortiz <guillermortiz21@gmail.com>
2021-01-18 17:19:29 -0600

    Create schema, create schema class, create object and add it to the database

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -3,9 +3,55 @@
 const mongoose = require('mongoose');
 // playground is the name of the db
 // mongo will create it automatically
-mongoose.connect('mongodb://localhost/playground', 
+mongoose.connect('mongodb://localhost:27017/playground', 
   {useNewUrlParser:true, useUnifiedTopology:true}
 )
   .then(() => console.log('Connected to mongodb'))
   .catch((err) => console.log(`Could not connect to mongodb...: ${err.message}`));
+
+// This schema will define the shape of the course documents inside mongo db
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: {type: Date, default: Date.now},
+  isPublished: Boolean
+});
+
+/*
+Possible types
+  String
+  Number
+  Date
+  Buffer
+  Boolean
+  ObjectID
+  Array
+*/
+
+// The schema is the form of a Class
+// The model is a Class that can have many objects
+// You need to compile the courseSchema into a model.
+// Params are: singular name and schema
+const Course = mongoose.model('Course', courseSchema);
+
+async function createCourse(){  
+  // Now we can create objects based on this class
+  // The param is the initializer
+  const course = new Course({
+    name: 'Angular Course',
+    author: 'Mosh',
+    tags: ['angular', 'frontend'],
+    isPublished: true
+  });
+  
+  // Now we can save this to the database.
+  
+  const result = await course.save();
+  // result has the document that was added to the database
+  console.log(result);
+}
+
+createCourse();
+
 
