c77961d67f87a3e8cd606e0f77f7158e4e5ca9a5
Guillermo Ortiz <guillermortiz21@gmail.com>
2021-01-17 20:02:35 -0600

    Create course

--- restful-api/index.js
+++ restful-api/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 const courses = [
   {id: 1, name: 'Course1'},
   {id: 2, name: 'Course2'},
@@ -23,6 +25,17 @@ app.get('/api/courses/:id', (req, res) => {
     course => course.id === parseInt(req.params.id)
   );
   if(!course) res.status(404).send(`The course with id ${req.params.id} was not found`);
+  res.send(course);
+});
+
+// Crea a course
+app.post('/api/courses', (req, res) => {
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name
+  };
+  courses.push(course);
+  // By convention, we respond the client with the created object
   res.send(course);
 });
 
