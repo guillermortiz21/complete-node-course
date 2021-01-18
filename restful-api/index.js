8e61fa02599e116e5fc4f1faf5a738c9c0e07cf9
Guillermo Ortiz <guillermortiz21@gmail.com>
2021-01-17 19:51:02 -0600

    Get all and find by id, 404 error

--- restful-api/index.js
+++ restful-api/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 
+const courses = [
+  {id: 1, name: 'Course1'},
+  {id: 2, name: 'Course2'},
+  {id: 3, name: 'Course3'},
+]
+
 // Lets do a get request
 
 app.get('/', (req, res) => {
@@ -8,12 +14,16 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api/courses', (req, res) => {
-  res.send([1,2,3]);
+  res.send(courses);
 });
 
 // Get a single course
 app.get('/api/courses/:id', (req, res) => {
-  res.send(req.params.id);
+  const course = courses.find(
+    course => course.id === parseInt(req.params.id)
+  );
+  if(!course) res.status(404).send(`The course with id ${req.params.id} was not found`);
+  res.send(course);
 });
 
 // Two params!
