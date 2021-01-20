349873e0dfe71a898704b810361a69356c3a123d
Guillermo Ortiz <guillermortiz21@gmail.com>
2021-01-19 18:43:09 -0600

    Bug fixed Cast to ObjectId Error

--- vidly/routes/customers.js
+++ vidly/routes/customers.js
@@ -38,21 +38,27 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   const {error} = validate(req.body);
   if(error) return res.status(400).send(`Error updating a customer: ${error.details[0].message}`);
-
-  const customer = await Customer.findByIdAndUpdate(
-    req.params.id, 
-    {name: req.body.name, isGold: req.body.isGold, phone: req.body.phone}, 
-    {useFindAndModify:false, new: true}
-  );
-  if(!customer) return res.status(404).send(`Customer with id ${req.params.id} was not found`);
-  res.send(customer);
+  try{
+    const customer = await Customer.findByIdAndUpdate(
+      req.params.id, 
+      {name: req.body.name, isGold: req.body.isGold, phone: req.body.phone}, 
+      {useFindAndModify:false, new: true}
+    );
+    if(!customer) return res.status(404).send(`Customer with id ${req.params.id} was not found`);
+    res.send(customer);
+  }catch(err){
+    return res.status(404).send(`Customer with id ${req.params.id} was not found`);
+  }
 });
 
 router.delete('/:id', async (req, res) => {
-  const customer = await Customer.findByIdAndRemove(req.params.id, {useFindAndModify:false});
-  if(!customer) return res.status(404).send(`Customer with id ${req.params.id} was not found`);
-
-  res.send(customer);
+  try{
+    const customer = await Customer.findByIdAndRemove(req.params.id, {useFindAndModify:false});
+    if(!customer) return res.status(404).send(`Customer with id ${req.params.id} was not found`);
+    res.send(customer);
+  }catch(err){
+    return res.status(404).send(`Customer with id ${req.params.id} was not found`);
+  }
 });
 
 const lookUpCustomer = async function(customerId){
--- vidly/routes/genres.js
+++ vidly/routes/genres.js
@@ -34,17 +34,23 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   const {error} = validate(req.body);
   if(error) return res.status(400).send(`Error updating a genre: ${error.details[0].message}`);
-
-  const genre = await Genre.findByIdAndUpdate(req.params.id, {name: req.body.name}, {useFindAndModify:false, new: true});
-  if(!genre) return res.status(404).send(`Genre with id ${req.params.id} was not found`);
-  res.send(genre);
+  try{
+    const genre = await Genre.findByIdAndUpdate(req.params.id, {name: req.body.name}, {useFindAndModify:false, new: true});
+    if(!genre) return res.status(404).send(`Genre with id ${req.params.id} was not found`);
+    res.send(genre);
+  }catch(err){
+    return res.status(404).send(`Genre with id ${req.params.id} was not found`);
+  }
 });
 
 router.delete('/:id', async (req, res) => {
-  const result = await Genre.findByIdAndRemove(req.params.id, {useFindAndModify:false});
-  if(!result) return res.status(404).send(`Genre with id ${req.params.id} was not found`);
-
-  res.send(result);
+  try{
+    const result = await Genre.findByIdAndRemove(req.params.id, {useFindAndModify:false});
+    if(!result) return res.status(404).send(`Genre with id ${req.params.id} was not found`);
+    res.send(result);
+  }catch(err){
+    return res.status(404).send(`Genre with id ${req.params.id} was not found`);
+  }
 });
 
 const lookUpGenre = async function(genreId){
