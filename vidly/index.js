c73c282ab58ef016f805d0afa8faa847b0070f8f
Guillermo Ortiz <guillermortiz21@gmail.com>
2021-01-27 20:27:29 -0600

    Separating route related code to routes.js file

--- vidly/index.js
+++ vidly/index.js
@@ -2,19 +2,18 @@ require('express-async-errors');
 const winston = require('winston');
 require('winston-mongodb');
 const config = require('config');
-const error = require('./middleware/error');
+
+const express = require('express');
+const app = express()
+
+// Start routes and middleware
+require('./startup/routes')(app);
+
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi)
 const mongoose = require('mongoose');
-const genres = require('./routes/genres');
-const customers = require('./routes/customers');
-const movies = require('./routes/movies');
-const rentals = require('./routes/rentals');
-const users = require('./routes/users');
-const auth = require('./routes/auth');
-const home = require('./routes/home');
-const express = require('express');
-const app = express();
+
+;
 
 if(!config.get('jwtPrivateKey')){
   console.log('FATAL ERROR: jwtPrivateKey is not defined');
@@ -51,18 +50,6 @@ mongoose.connect('mongodb://localhost:27017/vidly',
   .then(() => console.log('Connected to mongodb'))
   .catch((err) => console.log(`Could not connect to mongodb...: ${err.message}`));
 
-app.use(express.json());
-
-app.use('/', home);
-app.use('/api/genres', genres);
-app.use('/api/customers', customers);
-app.use('/api/movies', movies);
-app.use('/api/rentals', rentals);
-app.use('/api/users', users);
-app.use('/api/auth', auth);
-
-// Error middleware
-app.use(error);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Listening to port ${PORT}...`));
