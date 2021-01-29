9286edeafed20882ba32bd64c6c3aec6f9348511
Guillermo Ortiz <guillermortiz21@gmail.com>
2021-01-28 18:14:55 -0600

    Testing arrays

--- testing-demo/tests/lib.test.js
+++ testing-demo/tests/lib.test.js
@@ -27,5 +27,31 @@ describe('greet', () => {
     // Test not too specific, as long as we have Guillermo in the
     // message, it is okay.
     expect(result).toMatch(/Guillermo/);
+    // You could also use .toContain('Guillermo');
   });
 });
+
+describe('getCurrencies', () => {
+  it('should return supported currencies', () => {
+    const result = lib.getCurrencies();
+    
+    // Too general
+    //expect(result).toBeDefined();
+    //expect(result).not.toBeNull();
+
+    // Too specific
+    //expect(result[0]).toBe('USD');
+    //expect(result[1]).toBe('AUD');
+    //expect(result[2]).toBe('EUR');
+    //expect(result.length).toBe(3);
+
+    // Proper we, balanced
+    // Check for existence of an element
+    //expect(result).toContain('USD');
+    //expect(result).toContain('AUD');
+    //expect(result).toContain('EUR');
+    
+    // Instead of three assertions, just one
+    expect(result).toEqual(expect.arrayContaining(['EUR', 'USD', 'AUD']))
+  });
+})
