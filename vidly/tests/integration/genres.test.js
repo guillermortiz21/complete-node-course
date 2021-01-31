91e248df49de1d8b3b0e71b2da7b2dfc66275bcb
Guillermo Ortiz <guillermortiz21@gmail.com>
2021-01-30 21:08:44 -0600

    Test for getting a genre with a given id

--- vidly/tests/integration/genres.test.js
+++ vidly/tests/integration/genres.test.js
@@ -1,5 +1,6 @@
 const request = require('supertest');
 const {Genre} = require('../../models/genre');
+const mongoose = require('mongoose');
 
 let server;
 
@@ -33,6 +34,23 @@ describe('/api/genres', () => {
       // Check if the two genres were fetched from the db.
       expect(res.body.some(genre => genre.name === 'genre1')).toBeTruthy(); 
       expect(res.body.some(genre => genre.name === 'genre2')).toBeTruthy();
+    });
+  });
+
+  describe('GET /:id', () => {
+    it('should return a genre with a given id if the genre exists', async () => {
+      const genre = new Genre({name: 'genre1'});
+      await genre.save();
+
+      const res = await request(server).get(`/api/genres/${genre._id}`);
+
+      expect(res.status).toBe(200);
+      expect(res.body).toHaveProperty('name', genre.name);
+    });
+
+    it('should return a 404 error if the genre does not exist', async () =>{
+      const res = await request(server).get('/api/genres/1');
+      expect(res.status).toBe(404);
     });
   });
 });
