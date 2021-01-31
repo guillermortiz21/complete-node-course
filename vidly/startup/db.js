b52c611b2fb33336b5a898e8602c1a12100232cb
Guillermo Ortiz <guillermortiz21@gmail.com>
2021-01-31 13:02:50 -0600

    Remove use createIndexes warning

--- vidly/startup/db.js
+++ vidly/startup/db.js
@@ -3,6 +3,7 @@ const winston = require('winston');
 
 // Heres is all the code related to the database
 module.exports = function(){
+  mongoose.set('useCreateIndex',true);
   // playground is the name of the db
   // mongo will create it automatically
   mongoose.connect(process.env.DB, 
